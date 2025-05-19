4eb95eb7c0b44801e56abaa715a1587e0fcdcca3
smulewsk <61460041+smulewsk@users.noreply.github.com>
2025-05-19 21:47:06 +0200

    Add tmux-resurrect

--- install_env.sh
+++ install_env.sh
@@ -1,2 +1,5 @@
-git clone git@github.com:tmux-plugins/tmux-resurrect.git
+mkdir .tmux
+cd .tmux
+git clone https://github.com/tmux-plugins/tmux-resurrect
+cd ..
 wget https://raw.github.com/smulewsk/env/main/.tmux.conf
